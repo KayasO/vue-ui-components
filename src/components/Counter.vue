0572b1136da8567c2c834db45cc48f1a884d79e0
Okan Kayas <okan-kayas@web.de>
2021-11-11 21:17:04 +0100

    Added Counter component, without functionality

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="counter">
+    <h1>Counter</h1>
+    <p>Counter: {{ counter }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
